6b8d24959aef58921237317b0a78fc488e900b99
Kirill Karpov <karpov.kir.v@gmail.com>
2024-05-04 18:14:38 +0400

    Initial frontend setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# URL shortener
+
+A simple app to shorten URLs.
+
+This repo was created for the purpose of an article on Medium - [Simple React App with Vite, TypeScript, Linting & Formatting](https://medium.com/@karpov-kir).
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import classNames from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <div className={classNames.header}>Header!</div>
+      <div className={classNames.main}>Main!</div>
+    </>
+  );
+}
+
+export default App;
