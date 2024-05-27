1d0baa977a4b6ead0d54b86f034d9dabe2b1feee
Kirill Karpov <karpov.kir.v@gmail.com>
2024-05-27 20:55:43 +0400

    Add basic controls

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import classNames from "./App.module.css";
 function App() {
   return (
     <>
-      <div className={classNames.header}>Header!</div>
-      <div className={classNames.main}>Main!</div>
+      <div className={classNames.header}>URL Shortener</div>
+      <div className={classNames.main}>
+        <input type="text" placeholder="Long URL" />
+        <button>Shorten</button>
+      </div>
     </>
   );
 }
